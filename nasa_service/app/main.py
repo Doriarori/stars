1820c821938fc49a43961e5ea97c23149d877fd1
simon <99534089+Doriarori@users.noreply.github.com>
2024-04-29 19:16:37 +0300

    new

--- nasa_service/app/main.py
+++ nasa_service/app/main.py
@@ -1,36 +1,75 @@
 import uvicorn
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Form, Header
 import requests
+from keycloak import KeycloakOpenID
 
 app = FastAPI()
 
 NASA_API_KEY = "IqUcYN3j1nIgoalQuZQaduBJe14v1sPV7M1ozFSE"
 
+KEYCLOAK_URL = "http://keycloak:8080/"
+KEYCLOAK_CLIENT_ID = "testClient"
+KEYCLOAK_REALM = "testRealm"
+KEYCLOAK_CLIENT_SECRET = "**********"
+
+keycloak_openid = KeycloakOpenID(server_url=KEYCLOAK_URL,
+                                 client_id=KEYCLOAK_CLIENT_ID,
+                                 realm_name=KEYCLOAK_REALM,
+                                 client_secret_key=KEYCLOAK_CLIENT_SECRET)
+
+
+@app.post("/get_token")
+async def get_token(username: str = Form(...), password: str = Form(...)):
+    try:
+        token = keycloak_openid.token(grant_type=["password"],
+                                      username=username,
+                                      password=password)
+        return token
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=400, detail="Не удалось получить токен")
+
+
+def check_roles(token):
+    try:
+        token_info = keycloak_openid.introspect(token)
+        if "test" not in token_info["realm_access"]["roles"]:
+            raise HTTPException(status_code=403, detail="Access denied")
+        return token_info
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Invalid token or access denied")
 
 @app.get("/health", status_code=status.HTTP_200_OK)
-async def service_alive():
-    return {'message': 'Service alive'}
+async def service_alive(token: str = Header()):
+    if (check_roles(token)):
+        return {'message': 'service alive'}
+    else:
+        return "Wrong JWT Token"
 
 
 @app.get("/apod")
-async def astronomy_picture_of_the_day():
-    url = f"https://api.nasa.gov/planetary/apod?api_key={NASA_API_KEY}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
+async def astronomy_picture_of_the_day(token: str = Header()):
+    if (check_roles(token)):
+        url = f"https://api.nasa.gov/planetary/apod?api_key={NASA_API_KEY}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise HTTPException(status_code=400, detail="Error retrieving APOD from NASA API")
     else:
-        raise HTTPException(status_code=400, detail="Error retrieving APOD from NASA API")
-
+        return "Wrong JWT Token"
 
 @app.get("/neo")
-async def near_earth_objects(start_date: str, end_date: str):
-    url = f"https://api.nasa.gov/neo/rest/v1/feed?start_date={start_date}&end_date={end_date}&api_key={NASA_API_KEY}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
+async def near_earth_objects(start_date: str, end_date: str, token: str = Header()):
+    if (check_roles(token)):
+        url = f"https://api.nasa.gov/neo/rest/v1/feed?start_date={start_date}&end_date={end_date}&api_key={NASA_API_KEY}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise HTTPException(status_code=400, detail="Error retrieving NEO data from NASA API")
     else:
-        raise HTTPException(status_code=400, detail="Error retrieving NEO data from NASA API")
-
+        return "Wrong JWT Token"
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=80)
